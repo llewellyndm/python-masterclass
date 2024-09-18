6ebcc836239159d287b38c3a04e7803295f5db54
llewellyndm <llewellyn@themindlab.co.uk>
2024-09-18 15:12:05 +0100

    add title screen

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ HEIGHT = 600
 FPS = 60 # max fps if computer can handle it
 PLAYER_SPEED = 5
 BACKGROUND_SCROLL_SPEED = 2
+WHITE = (255, 255, 255)
 
 # Create a Pygame window and set its dimensions
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -44,20 +45,34 @@ player_rect = player.get_rect()
 player_rect.midleft = (25, HEIGHT // 2) # double slash for integer division
 
 # Enemies
-
 enemy_one = load_image("spaceship_en_one")
 enemy_two = load_image("spaceship_en_two")
 enemy_three = load_image("spaceship_en_three")
 enemy_four = load_image("spaceship_en_four")
 enemy_five = load_image("spaceship_en_five")
-
 enemy_images = [enemy_one, enemy_two, enemy_three, enemy_four, enemy_five]
 enemy_speed = 5
 enemy_spawn_rate = 3000
 last_enemy_spawn = 0
 enemies = []
 
+# Title screen
+title_font = pygame.font.Font(os.path.join("assets", "fonts", "LuckiestGuy-Regular.ttf"), 72)
+instruction_font = pygame.font.Font(os.path.join("assets", "fonts", "LuckiestGuy-Regular.ttf"), 32)
+title_string = "SPACE ATTACK!"
+instruction_string = "Press ENTER to start"
+title_text = title_font.render(title_string, True, WHITE) # middle param for anti-aliasing
+instruction_text = instruction_font.render(instruction_string, True, WHITE)
+title_text_rect = title_text.get_rect()
+title_text_rect.center = (WIDTH // 2, 120)
+instruction_text_rect = instruction_text.get_rect()
+instruction_text_rect.center = (WIDTH // 2, 480)
+title_image = player
+title_image_rect = title_image.get_rect()
+title_image_rect.center = (WIDTH // 2, HEIGHT // 2)
+
 # Main game loop
+game_over = True
 running = True
 while running:
     # Check for events
@@ -65,56 +80,65 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Game logic
-    current_time = pygame.time.get_ticks()
+    if not game_over:
+        # Game logic
+        current_time = pygame.time.get_ticks()
 
-    # Background scrolling
-    if background_rect_one.x < -800:
-        background_rect_one.x = WIDTH
-    if background_rect_two.x < -800:
-        background_rect_two.x = WIDTH
+        # Background scrolling
+        if background_rect_one.x < -800:
+            background_rect_one.x = WIDTH
+        if background_rect_two.x < -800:
+            background_rect_two.x = WIDTH
 
-    background_rect_one.x -= BACKGROUND_SCROLL_SPEED
-    background_rect_two.x -= BACKGROUND_SCROLL_SPEED
+        background_rect_one.x -= BACKGROUND_SCROLL_SPEED
+        background_rect_two.x -= BACKGROUND_SCROLL_SPEED
 
-    # Player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and player_rect.top > 0:
-        player_rect.y -= PLAYER_SPEED
-    if keys[pygame.K_DOWN] and player_rect.bottom < HEIGHT:
-        player_rect.y += PLAYER_SPEED
+        # Player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and player_rect.top > 0:
+            player_rect.y -= PLAYER_SPEED
+        if keys[pygame.K_DOWN] and player_rect.bottom < HEIGHT:
+            player_rect.y += PLAYER_SPEED
 
-    # Enemy movement
-    for enemy_image, enemy_rect in enemies:
-        enemy_rect.x -= enemy_speed
-    
-    enemies = [(enemy_image, enemy_rect) for enemy_image, enemy_rect in enemies if enemy_rect.right > 0]
+        # Enemy movement
+        for enemy_image, enemy_rect in enemies:
+            enemy_rect.x -= enemy_speed
         
+        enemies = [(enemy_image, enemy_rect) for enemy_image, enemy_rect in enemies if enemy_rect.right > 0]
+            
 
-    # Enemy spawning
-    if current_time - last_enemy_spawn > enemy_spawn_rate:
-        enemy_image = random.choice(enemy_images)
-        enemy_rect = enemy_image.get_rect()
-        enemy_rect.x = (WIDTH + enemy_rect.width)
-        lane = random.randint(1, 3)
-        if lane == 1:
-            enemy_rect.y = 0
-        elif lane == 2:
-            enemy_rect.y = (HEIGHT // 2) - (enemy_rect.height // 2)
-        else:
-            enemy_rect.y = HEIGHT - enemy_rect.height
-        enemies.append((enemy_image, enemy_rect))
-        last_enemy_spawn = current_time
+        # Enemy spawning
+        if current_time - last_enemy_spawn > enemy_spawn_rate:
+            enemy_image = random.choice(enemy_images)
+            enemy_rect = enemy_image.get_rect()
+            enemy_rect.x = (WIDTH + enemy_rect.width)
+            lane = random.randint(1, 3)
+            if lane == 1:
+                enemy_rect.y = 0
+            elif lane == 2:
+                enemy_rect.y = (HEIGHT // 2) - (enemy_rect.height // 2)
+            else:
+                enemy_rect.y = HEIGHT - enemy_rect.height
+            enemies.append((enemy_image, enemy_rect))
+            last_enemy_spawn = current_time
 
 
-    # Draw surfaces 
-    # screen.fill("#4c43ea") # if you want to fill with single colour
-    # draws the following in order i.e. bgs first, then player
-    screen.blit(background, background_rect_one)
-    screen.blit(background, background_rect_two)
-    screen.blit(player, player_rect)
-    for enemy_image, enemy_rect in enemies:
-        screen.blit(enemy_image, enemy_rect)
+        # Draw surfaces 
+        # screen.fill("#4c43ea") # if you want to fill with single colour
+        # draws the following in order i.e. bgs first, then player
+        screen.blit(background, background_rect_one)
+        screen.blit(background, background_rect_two)
+        screen.blit(player, player_rect)
+        for enemy_image, enemy_rect in enemies:
+            screen.blit(enemy_image, enemy_rect)
+    else:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            game_over = False
+        screen.blit(background, (0, 0))
+        screen.blit(title_text, title_text_rect)
+        screen.blit(instruction_text, instruction_text_rect)
+        screen.blit(title_image, title_image_rect)
 
     # Update display
     pygame.display.update()
