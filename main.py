93601c795beced5aea9d70d31ee61fc8585cd4df
llewellyndm <llewellyn@themindlab.co.uk>
2024-09-18 11:22:46 +0100

    draw background + player ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 
 # Initialize Pygame
 pygame.init()
@@ -14,6 +15,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Set the window title
 pygame.display.set_caption("Space Attack")
 
+# Get background image
+# building path manually instead of hardcoding here for cross-platform support
+background = pygame.image.load(os.path.join("assets", "images", "space_bg.png"))
+
+# Player
+player = pygame.image.load(os.path.join("assets", "images", "spaceship_pl.png"))
+player_rect = player.get_rect()
+player_rect.midleft = (25, HEIGHT // 2) # double slash for integer division
+
 # Main game loop
 running = True
 while running:
@@ -25,9 +35,12 @@ while running:
     # Game logic 
 
     # Draw surfaces 
-    screen.fill("red")
+    # screen.fill("#4c43ea") # if you want to fill with single colour
+    # draws the following in order i.e. bg first, then player
+    screen.blit(background, (0, 0))
+    screen.blit(player, player_rect)
 
-    # Update dispay
+    # Update display
     pygame.display.update()
 
 # Quit Pygame
